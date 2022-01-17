bc75eb60538f91b36e355f940e7bd8e48fccd5a8
annie-kuo <97212916+annie-kuo@users.noreply.github.com>
2022-01-17 16:20:41 -0500

    Update zoomrecs.h

--- zoomrecs.h
+++ zoomrecs.h
@@ -1,10 +1,5 @@
 /*
 Program to generate an ordered list of student lab attendance
-***************************************************************
-* Author	Dept.		Date		Notes
-***************************************************************
-* Annie K	Comp. Science.	Apr 26 2021	Inital blank version.
-* Annie K	Comp. Science.	Apr 27 2021	Added
 */
 
 #include <stdio.h>
